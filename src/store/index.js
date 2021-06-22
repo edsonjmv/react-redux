b030b2ca6dd06fd06580d9b4b0648202a38fc4ee
Edson Machado <edsonjmv@gmail.com>
2021-06-22 12:11:33 +0200

    attaching payloads to actions

--- src/store/index.js
+++ src/store/index.js
@@ -6,11 +6,19 @@ const counterReducer = (state = { counter: 0 }, action) => {
       counter: state.counter + 1
     };
   }
+
+  if (action.type === 'increase') {
+    return {
+      counter: state.counter + action.amount
+    };
+  }
+
   if (action.type === 'decrement') {
     return {
       counter: state.counter - 1
     };
   }
+
   return state;
 };
 
